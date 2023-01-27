2eddb2240863d7e7ce93b1cfc59187daf7b7dd10
Mohammed Taitai <mohammedtaitai@outlook.com>
2023-01-27 17:18:59 -0500

    commit tree 9202f4ef2bb9c0f26a2976a59a8a1655e3c815a2

---
+++ lib/insert.sql
@@ -0,0 +1,23 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Mr.Chocolate', 23,'M', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Rowdy', 23,'F', 'black', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Tabitha', 23,'M', 'white', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Sergeant Brown', 23,'F', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Melissa', 23,'M', 'white', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Grinch', 23,'M', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES('Wendy', 23,'F', 'brown', 'calm', 1);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive)
+VALUES(NULL, 23,'M', 'brown', 'calm', 1);
